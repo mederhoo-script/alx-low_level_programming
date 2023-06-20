8074d747f4a2377561fe0192fc886779ec70453f
HAMMED ORIYOMI AMUSAT <mederhoo@yahoo.com>
2023-06-20 08:12:20 -0700

    alphabet in small letter

---
+++ 0x02-functions_nested_loops/1-alphabet.c
@@ -0,0 +1,16 @@
+#include "main.h"
+
+/**
+ * print_alphabet - Prints the alphabet in lowercase
+ */
+void print_alphabet(void)
+{
+	char l;
+
+	for (l = 'a'; l <= 'z'; l++)
+	{
+		_putchar(l);
+	}
+
+	_putchar('\n');
+}
