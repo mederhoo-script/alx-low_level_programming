12e84b59f99d8881f56ab6b9fcd779f256c8acc4
HAMMED ORIYOMI AMUSAT <mederhoo@yahoo.com>
2023-06-22 14:17:55 -0700

    0-9 without 2 and 4

---
+++ 0x04-more_functions_nested_loops/4-print_most_numbers.c
@@ -0,0 +1,20 @@
+#include"main.h"
+
+/**
+ * print_most_numbers - print 0 - 9 apart
+ *                 from 2 and 4
+ * Return:  0
+ */
+
+void print_most_numbers(void)
+{
+	int i;
+
+	for (i = 0; i < 10; ++i)
+	{
+		if (i == 2 || i == 4)
+			continue;
+		_putchar(i + 48);
+	}
+	_putchar('\n');
+}
