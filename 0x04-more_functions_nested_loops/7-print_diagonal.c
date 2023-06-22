f21c7c29dded2b79acb721996b2320c3b03e0d57
HAMMED ORIYOMI AMUSAT <mederhoo@yahoo.com>
2023-06-22 16:34:16 -0700

    diagonal

--- 0x04-more_functions_nested_loops/7-print_diagonal.c
+++ 0x04-more_functions_nested_loops/7-print_diagonal.c
@@ -3,7 +3,8 @@
 /**
  * print_diagonal - print a diagonal line
  *
- * @n: is the number of times of /
+ * @n: is the number of times the \ character
+ *     should be printed
  */
 
 void print_diagonal(int n)
@@ -18,7 +19,7 @@ void print_diagonal(int n)
 		{
 			for (space = 1; space <= postn; ++space)
 				_putchar(' ');
-			_putchar(92); /*is same as '/' char*/
+			_putchar(92); /*is equal to '/' char*/
 			_putchar('\n');
 		}
 	}
