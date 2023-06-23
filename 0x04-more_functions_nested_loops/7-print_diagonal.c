864ef59715ef7bcdd6e079bfbed3adbf448bd715
HAMMED ORIYOMI AMUSAT <mederhoo@yahoo.com>
2023-06-22 17:06:48 -0700

    print squre

--- 0x04-more_functions_nested_loops/7-print_diagonal.c
+++ 0x04-more_functions_nested_loops/7-print_diagonal.c
@@ -1,26 +1,23 @@
-#include"main.h"
-
 /**
- * print_diagonal - print a diagonal line
- *
- * @n: is the number of times the \ character
- *     should be printed
+ * print_diagonal - Draws a diagonal line on the terminal
+ * @n: Number of times the character '\' should be printed
  */
-
 void print_diagonal(int n)
 {
-	int postn, space;
+	int i, j;
 
 	if (n <= 0)
-		_putchar('\n');
-	else
 	{
-		for (postn = 1; postn <= n; ++postn)
-		{
-			for (space = 1; space <= postn; ++space)
-				_putchar(' ');
-			_putchar(92); /*is equal to '/' char*/
-			_putchar('\n');
-		}
+		_putchar('\n');
+		return;
+	}
+
+	for (i = 0; i < n; i++)
+	{
+		for (j = 0; j < i; j++)
+			_putchar(' ');
+
+		_putchar('\\');
+		_putchar('\n');
 	}
 }
