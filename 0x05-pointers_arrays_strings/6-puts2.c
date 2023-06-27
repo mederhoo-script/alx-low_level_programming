e2fc1e542d75df52bccf74e090d23858a4d2df59
HAMMED ORIYOMI AMUSAT <mederhoo@yahoo.com>
2023-06-26 19:21:53 -0700

    6 puts

---
+++ 0x05-pointers_arrays_strings/6-puts2.c
@@ -0,0 +1,17 @@
+#include "main.h"
+/**
+ * puts2 - prints every other character
+ *
+ * @str: char
+ *mederhoo
+ * Return: 0 is
+ */
+void puts2(char *str)
+{
+	int s;
+
+	for (s = 0; str[s] != '\0'; s++)
+		if (s % 2 == 0)
+			_putchar(str[s]);
+	_putchar('\n');
+}
