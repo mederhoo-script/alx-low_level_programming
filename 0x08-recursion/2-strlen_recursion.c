ad063252610b7795d3492a615a9c0a7ed4267e15
HAMMED ORIYOMI AMUSAT <mederhoo@yahoo.com>
2023-07-05 19:50:58 -0700

    lenght checker like strlen

---
+++ 0x08-recursion/2-strlen_recursion.c
@@ -0,0 +1,20 @@
+#include <stdio.h>
+
+/**
+ * _strlen_recursion - Returns the length of a string using recursion
+ * @s: Pointer to the string
+ *
+ * Return: Length of the string
+ */
+int _strlen_recursion(char *s)
+{
+	/* Base case: If the current character is the null character, return 0 */
+	if (*s == '\0')
+		return (0);
+
+	/*
+	 * Recursive call: Move to the next character and call itself recursively,
+	 * then add 1 to the result
+	 */
+	return (1 + _strlen_recursion(s + 1));
+}
