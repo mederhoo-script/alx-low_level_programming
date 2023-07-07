a8eb3182856eb1d1b0ba5b27843e7dc429a5dadf
HAMMED ORIYOMI AMUSAT <mederhoo@yahoo.com>
2023-07-06 18:00:09 -0700

    101 updare

--- 0x08-recursion/101-wildcmp.c
+++ 0x08-recursion/101-wildcmp.c
@@ -1,24 +1,20 @@
-#include <stdio.h>
+#include "main.h"
 
 /**
- * wildcmp - Compares two strings and checks for a match with wildcard *
- * @s1: First string
- * @s2: Second string
- *
- * Return: 1 if strings can be considered identical, 0 otherwise
+ * wildcmp - for compairisym.
+ * @s1: firs string .
+ * @s2: second string. It can contains a *.
+ * Return: 1 .
  */
 int wildcmp(char *s1, char *s2)
 {
+	if (*s2 == '*' && *(s2 + 1) != '\0' && *s1 == '\0')
+		return (0);
 	if (*s1 == '\0' && *s2 == '\0')
 		return (1);
-
-	if (*s1 == *s2 || *s2 == '*')
-	{
-		if (*(s2 + 1) == '*')
-			return (wildcmp(s1, s2 + 1) || wildcmp(s1 + 1, s2));
-		else
-			return (wildcmp(s1 + 1, s2 + 1));
-	}
-
+	if (*s1 == *s2)
+		return (wildcmp(s1 + 1, s2 + 1));
+	if (*s2 == '*')
+		return (wildcmp(s1, s2 + 1) || wildcmp(s1 + 1, s2));
 	return (0);
 }
