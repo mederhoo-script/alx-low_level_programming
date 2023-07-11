d91a2d1ae27e87cc620ac846dc7f27a591381004
hammed amusat <mederhoo@gmail.com>
2023-07-11 21:26:42 +0100

    update

--- 0x0A-argc_argv/0-whatsmyname.c
+++ 0x0A-argc_argv/0-whatsmyname.c
@@ -6,7 +6,7 @@
  * @argc: The number of command-line arguments
  * @argv: An array of strings representing the command-line arguments
  *
- * Return: Always 0
+ * Return: 0
  */
 int main(int argc, char *argv[])
 	(void)argc;
