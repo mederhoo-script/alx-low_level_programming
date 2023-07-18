d255716b47049ef020b81d3275f823270e091681
HAMMED ORIYOMI AMUSAT <mederhoo@yahoo.com>
2023-07-18 07:38:46 -0700

    intializ dog

--- 0x0E-structures_typedef/dog.h
+++ 0x0E-structures_typedef/dog.h
@@ -20,10 +20,10 @@ struct dog
 
 void init_dog(struct dog *d, char *name, float age, char *owner);/*task 1*/
 
-/*void print_dog(struct dog *d);task 2*/
+void print_dog(struct dog *d);/*task 2*/
 
-/*dog_t *new_dog(char *name, float age, char *owner);task 4*/
+dog_t *new_dog(char *name, float age, char *owner);/*task 4*/
 
-/*void free_dog(dog_t *d);task 5 prototype*/
+void free_dog(dog_t *d);/*task 5 prototype*/
 
 #endif/*mederhoo-script*/
