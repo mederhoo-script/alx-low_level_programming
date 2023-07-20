2d1d775acde7917cf7e4b2b073e12c22e5535b7c
HAMMED ORIYOMI AMUSAT <mederhoo@yahoo.com>
2023-07-20 08:53:20 -0700

    print all updated

--- 0x10-variadic_functions/3-print_all.c
+++ 0x10-variadic_functions/3-print_all.c
@@ -10,18 +10,29 @@ void print_all(const char * const format, ...)
 {
 	va_list args;
 	int i = 0;
-	char *str, c;
+	char *str;
+	char c;
+	int integer;
+	float floating;
 
 	va_start(args, format);
 	while (format && format[i])
 	{
 		c = format[i++];
 		if (c == 'c')
+		{
 			printf("%c", va_arg(args, int));
+		}
 		else if (c == 'i')
-			printf("%d", va_arg(args, int));
+		{
+			integer = va_arg(args, int);
+			printf("%d", integer);
+		}
 		else if (c == 'f')
-			printf("%f", va_arg(args, double));
+		{
+			floating = va_arg(args, double);
+			printf("%f", floating);
+		}
 		else if (c == 's')
 		{
 			str = va_arg(args, char *);
