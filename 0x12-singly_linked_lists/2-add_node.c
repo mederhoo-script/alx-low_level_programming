76de074c083180ef0b84f6494b41ef2911638ab7
HAMMED ORIYOMI AMUSAT <mederhoo@yahoo.com>
2023-07-27 07:15:28 -0700

    update

--- 0x12-singly_linked_lists/2-add_node.c
+++ 0x12-singly_linked_lists/2-add_node.c
@@ -7,9 +7,11 @@
  * @s: Pointer to the string.
  * Return: The length of the string.
  */
-size_t _strlen(const char *s)
+int _strlen(const char *s)
 {
-	size_t length = 0;
+	int length = 0; /* Variable to store the length of the string */
+
+	/* Loop through the string until the null terminator is encountered */
 	while (s[length] != '\0')
 	{
 		length++;
@@ -26,13 +28,18 @@ size_t _strlen(const char *s)
  */
 list_t *add_node(list_t **head, const char *str)
 {
+	/* Check if the input string is NULL */
 	if (str == NULL)
 		return (NULL);
 
-	list_t *new_node = malloc(sizeof(list_t));
+	list_t *new_node; /* Pointer to the new node */
+
+	/* Allocate memory for the new node */
+	new_node = malloc(sizeof(list_t));
 	if (new_node == NULL)
 		return (NULL);
 
+	/* Duplicate the input string and store it in the new node */
 	new_node->str = strdup(str);
 	if (new_node->str == NULL)
 	{
@@ -40,7 +47,10 @@ list_t *add_node(list_t **head, const char *str)
 		return (NULL);
 	}
 
+	/* Calculate the length of the string and store it in the new node */
 	new_node->len = _strlen(str);
+
+	/* Insert the new node at the beginning of the list */
 	new_node->next = *head;
 	*head = new_node;
 
