06bd2feec707c9a42fe792a649546b1220992613
HAMMED ORIYOMI AMUSAT <mederhoo@yahoo.com>
2023-07-27 06:05:09 -0700

    print list t updade

--- 0x12-singly_linked_lists/0-print_list.c
+++ 0x12-singly_linked_lists/0-print_list.c
@@ -9,9 +9,10 @@
 size_t print_list(const list_t *h)
 {
 	size_t count = 0;
+
 	while (h != NULL)
 	{
-		printf("%s\n", h->str);
+		printf("[%d] %s\n", h->len, h->str);
 		h = h->next;
 		count++;
 	}
