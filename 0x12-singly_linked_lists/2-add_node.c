5d9e224e875ce8904e7de6008a16e6afc55a6589
HAMMED ORIYOMI AMUSAT <mederhoo@yahoo.com>
2023-07-27 07:01:21 -0700

    update

--- 0x12-singly_linked_lists/2-add_node.c
+++ 0x12-singly_linked_lists/2-add_node.c
@@ -3,6 +3,21 @@
 #include "lists.h"
 
 /**
+ * _strlen - Returns the length of a string.
+ * @s: Pointer to the string.
+ * Return: The length of the string.
+ */
+size_t _strlen(const char *s)
+{
+	size_t length = 0;
+	while (s[length] != '\0')
+	{
+		length++;
+	}
+	return (length);
+}
+
+/**
  * add_node - Adds a new node at the beginning of a list_t list.
  * @head: Pointer to a pointer to the head of the list.
  * @str: String to be stored in the new node.
@@ -25,7 +40,7 @@ list_t *add_node(list_t **head, const char *str)
 		return (NULL);
 	}
 
-	new_node->len = strlen(str);
+	new_node->len = _strlen(str);
 	new_node->next = *head;
 	*head = new_node;
 
