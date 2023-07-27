ea6ce2c2739817acee573d0a2c0733517562604f
HAMMED ORIYOMI AMUSAT <mederhoo@yahoo.com>
2023-07-27 11:47:53 -0700

    64-bit program in assembly that prints Hello, Holberton, followed by a new line.

---
+++ 0x12-singly_linked_lists/101-hello_holberton.asm
@@ -0,0 +1,24 @@
+	section .data
+	hello_msg db 'Hello, Holberton', 0
+	format db '%s', 0
+
+	section .text
+	global main
+
+	extern printf
+
+main:
+	;;  Set up the stack frame (not necessary for this simple program)
+	push rbp
+	mov rbp, rsp
+
+	;;  Call printf to print the message
+	lea rdi, [hello_msg] ; Load address of hello_msg into rdi (1st argument for printf)
+	lea rsi, [format]	; Load address of format string into rsi (2nd argument for printf)
+	xor rax, rax	; Clear rax to indicate no floating-point arguments (printf uses xmm registers)
+	call printf	; Call the printf function
+
+	;;  Clean up and return
+	mov rsp, rbp
+	pop rbp
+	ret
