458d821065b9add07a6f0e3858a5a3b9e246fb08
HAMMED ORIYOMI AMUSAT <mederhoo@yahoo.com>
2023-07-31 13:14:05 -0700

    function pop list int and get node int update

--- 0x13-more_singly_linked_lists/6-pop_listint.c
+++ 0x13-more_singly_linked_lists/6-pop_listint.c
@@ -10,16 +10,16 @@
  */
 int pop_listint(listint_t **head)
 {
-    int data;
-    listint_t *temp;
+	int data;
+	listint_t *temp;
 
-    if (head == NULL || *head == NULL)
-        return (0);
+	if (head == NULL || *head == NULL)
+		return (0);
 
-    temp = *head;
-    data = temp->n;
-    *head = (*head)->next;
-    free(temp);
+	temp = *head;
+	data = temp->n;
+	*head = (*head)->next;
+	free(temp);
 
-    return (data);
+	return (data);
 }
--- 0x13-more_singly_linked_lists/7-get_nodeint.c
+++ 0x13-more_singly_linked_lists/7-get_nodeint.c
@@ -10,16 +10,16 @@
  */
 listint_t *get_nodeint_at_index(listint_t *head, unsigned int index)
 {
-    listint_t *current = head;
-    unsigned int count = 0;
+	listint_t *current = head;
+	unsigned int count = 0;
 
-    while (current != NULL)
-    {
-        if (count == index)
-            return (current);
-        count++;
-        current = current->next;
-    }
+	while (current != NULL)
+	{
+		if (count == index)
+			return (current);
+		count++;
+		current = current->next;
+	}
 
-    return (NULL);
+	return (NULL);
 }
