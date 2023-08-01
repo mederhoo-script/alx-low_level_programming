e094034c53db2abeb04e40c21a404e103e12c81c
HAMMED ORIYOMI AMUSAT <mederhoo@yahoo.com>
2023-08-01 10:14:01 -0700

    function that prints a listint_t linked list.

--- 0x13-more_singly_linked_lists/101-print_listint_safe.c
+++ 0x13-more_singly_linked_lists/101-print_listint_safe.c
@@ -3,31 +3,46 @@
 #include "lists.h"
 
 /**
- * print_listint_safe - Prints a listint_t linked list safely.
- * @head: Pointer to the head of the list.
- *
+ * print_listint_safe - Prints a listint_t linked list.
+ * @head: Pointer to the beginning of the list.
  * Return: The number of nodes in the list.
  */
 size_t print_listint_safe(const listint_t *head)
 {
 	size_t count = 0;
-	const listint_t *current = head;
-	const listint_t *temp = NULL;
+	const listint_t *slow, *fast;
 
-	while (current != NULL)
+	slow = head;
+	fast = head;
+
+	while (fast != NULL && fast->next != NULL)
 	{
-		printf("[%p] %d\n", (void *)current, current->n);
+		printf("[%p] %d\n", (void *)slow, slow->n);
 		count++;
+		slow = slow->next;
+		fast = fast->next->next;
 
-		temp = current;
-		current = current->next;
-
-		if (temp <= current)
+		/* Check for a loop */
+		if (slow == fast)
 		{
-			printf("-> [%p] %d\n", (void *)current, current->n);
+			/* Move slow to the head and find the start of the loop */
+			slow = head;
+			while (slow != fast)
+			{
+				slow = slow->next;
+				fast = fast->next;
+			}
+			printf("-> [%p] %d\n", (void *)slow, slow->n);
 			exit(98);
 		}
 	}
 
-	return (count);
+	/* Print the last node (if it exists) and increment the count */
+	if (slow != NULL)
+	{
+		printf("[%p] %d\n", (void *)slow, slow->n);
+		count++;
+	}
+
+	return count;
 }
