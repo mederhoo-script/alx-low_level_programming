01a6270ed8f0e44390a9219b730659935ff12fa5
mederhoo <mederhoo@mederhoo.kali.com>
2023-10-15 11:49:29 +0100

    print all list inside a linked list

--- 0x17-doubly_linked_lists/0-print_dlistint.c
+++ 0x17-doubly_linked_lists/0-print_dlistint.c
@@ -2,6 +2,13 @@
 #include <stdlib.h>
 #include "lists.h"
 
+/**
+ * print_dlistint - function that print all data
+ * @h: head parameter
+ *
+ * Return: size_t
+*/
+
 size_t print_dlistint(const dlistint_t *h)
 {
 size_t i = 0;
