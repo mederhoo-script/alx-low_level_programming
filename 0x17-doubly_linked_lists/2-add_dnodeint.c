82ab2c21a54aff2d3e3b93c17e5321f44ad37f00
mederhoo <mederhoo@mederhoo.kali.com>
2023-10-15 20:53:29 +0100

    add new

--- 0x17-doubly_linked_lists/2-add_dnodeint.c
+++ 0x17-doubly_linked_lists/2-add_dnodeint.c
@@ -13,10 +13,7 @@ dlistint_t *add_dnodeint(dlistint_t **head, const int n)
 dlistint_t *new;
 new = malloc(sizeof(dlistint_t));
 if (new == NULL)
-{
-dprintf(2, "error");
-exit(0);
-}
+return (NULL);
 
 new->n = n;
 if (*head != NULL)
