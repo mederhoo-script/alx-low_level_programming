fd3d1088fde20115f2f58614fbad883fa9fdd868
hammed amusat oriyomi <mederhoo@gmail.com>
2023-10-16 18:18:38 -0700

    operators

---
+++ 0x18-dynamic_libraries/100-ope-func.c
@@ -0,0 +1,54 @@
+#include <stdio.h>
+
+/**
+ * add - add
+ * @a: a
+ * @b: b
+ *
+ * Return: int
+ */
+	int add(a, b)
+	{
+		return (a + b);
+	}
+/**
+ * sub - su
+ * @a: a
+ * @b: b
+ * Return: int
+ */
+	int sub(a, b)
+	{
+		return (a - b);
+	}
+/**
+ * mul - multiply
+ * mod - module
+ * @a: a
+ * @b: b
+ * Return: int
+ */
+	int mul(a, b)
+	{
+		return (a *b);
+	}
+/**
+ * div - module
+ * @a: a
+ * @b: b
+ * Return: int
+ */
+	int div(a, b)
+	{
+		return (a / b);
+	}
+/**
+ * mod - module
+ * @a: a
+ * @b: b
+ * Return: int
+ */
+	int mod(a, b)
+	{
+		return (a % b);
+	}
